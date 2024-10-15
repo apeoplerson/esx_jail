f57023ffebab327f16dd4556d93d90923064e15c
apeoplerson <126418234+apeoplerson@users.noreply.github.com>
2024-10-15 18:28:48 -0400

    Update main.lua
    Updated for oxmysql

--- server/main.lua
+++ server/main.lua
@@ -1,12 +1,7 @@
-ESX = nil
 local playersInJail = {}
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
-	MySQL.Async.fetchAll('SELECT jail_time FROM users WHERE identifier = @identifier', {
-		['@identifier'] = xPlayer.identifier
-	}, function(result)
+	exports.oxmysql:execute('SELECT jail_time FROM users WHERE identifier = ?', { xPlayer.identifier }, function(result)
 		if result[1] and result[1].jail_time > 0 then
 			TriggerEvent('esx_jail:sendToJail', xPlayer.source, result[1].jail_time, true)
 		end
@@ -17,7 +12,8 @@ AddEventHandler('esx:playerDropped', function(playerId, reason)
 	playersInJail[playerId] = nil
 end)
 
-MySQL.ready(function()
+-- Use 'AddEventHandler' to listen for the 'oxmysql:ready' event
+AddEventHandler('oxmysql:ready', function()
 	Citizen.Wait(2000)
 	local xPlayers = ESX.GetPlayers()
 
@@ -25,9 +21,7 @@ MySQL.ready(function()
 		Citizen.Wait(100)
 		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
 
-		MySQL.Async.fetchAll('SELECT jail_time FROM users WHERE identifier = @identifier', {
-			['@identifier'] = xPlayer.identifier
-		}, function(result)
+		exports.oxmysql:execute('SELECT jail_time FROM users WHERE identifier = ?', { xPlayer.identifier }, function(result)
 			if result[1] and result[1].jail_time > 0 then
 				TriggerEvent('esx_jail:sendToJail', xPlayer.source, result[1].jail_time, true)
 			end
@@ -54,19 +48,15 @@ AddEventHandler('esx_jail:sendToJail', function(playerId, jailTime, quiet)
 
 	if xPlayer then
 		if not playersInJail[playerId] then
-			MySQL.Async.execute('UPDATE users SET jail_time = @jail_time WHERE identifier = @identifier', {
-				['@identifier'] = xPlayer.identifier,
-				['@jail_time'] = jailTime
-			}, function(rowsChanged)
+			exports.oxmysql:execute('UPDATE users SET jail_time = ? WHERE identifier = ?', { jailTime, xPlayer.identifier }, function(rowsChanged)
 				xPlayer.triggerEvent('esx_policejob:unrestrain')
 				xPlayer.triggerEvent('esx_jail:jailPlayer', jailTime)
 				playersInJail[playerId] = {timeRemaining = jailTime, identifier = xPlayer.getIdentifier()}
 
 				if not quiet then
-					TriggerClientEvent('chat:addMessage', -1, {args = {_U('judge'), _U('jailed_msg', xPlayer.getName(), ESX.Math.Round(jailTime / 60))}, color = {147, 196, 109}})
+					TriggerClientEvent('chat:addMessage', -1, {args = {'Judge', ('%s has been jailed for %s minutes.'):format(xPlayer.getName(), ESX.Math.Round(jailTime / 60))}, color = {147, 196, 109}})
 				end
 			end)
-
 		end
 	end
 end)
@@ -76,10 +66,8 @@ function unjailPlayer(playerId)
 
 	if xPlayer then
 		if playersInJail[playerId] then
-			MySQL.Async.execute('UPDATE users SET jail_time = 0 WHERE identifier = @identifier', {
-				['@identifier'] = xPlayer.identifier
-			}, function(rowsChanged)
-				TriggerClientEvent('chat:addMessage', -1, {args = {_U('judge'), _U('unjailed', xPlayer.getName())}, color = {147, 196, 109}})
+			exports.oxmysql:execute('UPDATE users SET jail_time = 0 WHERE identifier = ?', { xPlayer.identifier }, function(rowsChanged)
+				TriggerClientEvent('chat:addMessage', -1, {args = {'Judge', ('%s has been released from jail.'):format(xPlayer.getName())}, color = {147, 196, 109}})
 				playersInJail[playerId] = nil
 				xPlayer.triggerEvent('esx_jail:unjailPlayer')
 			end)
@@ -91,34 +79,24 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1000)
 
-		for playerId,data in pairs(playersInJail) do
+		for playerId, data in pairs(playersInJail) do
 			playersInJail[playerId].timeRemaining = data.timeRemaining - 1
 
 			if data.timeRemaining < 1 then
-				unjailPlayer(playerId, false)
+				unjailPlayer(playerId)
 			end
 		end
 	end
 end)
 
+-- Updated thread without 'Async.parallelLimit'
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(Config.JailTimeSyncInterval)
-		local tasks = {}
 
-		for playerId,data in pairs(playersInJail) do
-			local task = function(cb)
-				MySQL.Async.execute('UPDATE users SET jail_time = @time_remaining WHERE identifier = @identifier', {
-					['@identifier'] = data.identifier,
-					['@time_remaining'] = data.timeRemaining
-				}, function(rowsChanged)
-					cb(rowsChanged)
-				end)
-			end
-
-			table.insert(tasks, task)
+		for playerId, data in pairs(playersInJail) do
+			-- Update jail time for each player
+			exports.oxmysql:execute('UPDATE users SET jail_time = ? WHERE identifier = ?', { data.timeRemaining, data.identifier })
 		end
-
-		Async.parallelLimit(tasks, 4, function(results) end)
 	end
 end)
